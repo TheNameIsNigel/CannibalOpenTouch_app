5ab4a95979c5e389fdfd387bca676428e66be611
Drew Walton <drewwalton@cinnarch.localnet>
2012-10-19 15:08:25 -0600

    Display current settings on app launch

--- src/com/poc/recoveryconfig/MainActivity.java
+++ src/com/poc/recoveryconfig/MainActivity.java
@@ -1,11 +1,15 @@
 package com.poc.recoveryconfig;
 
+import com.poc.recoveryconfig.util.IniStreamReader;
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -16,7 +20,6 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,33 +43,7 @@ public class MainActivity extends Activity {
         goowipebtn.setOnClickListener(gooWipeListener);
         goobootbtn.setOnClickListener(gooBootListener);
         choosenandlocbtn.setOnClickListener(chooseNandListener);
-        String fileName = ".userdefinedbackups";
-        File sdDir = new File(Environment.getExternalStorageDirectory().getPath());
-        File readFile = new File(sdDir.getPath() + "/cotrecovery/" + fileName);
-        TextView t = new TextView(this);
-		t = (TextView)findViewById(R.id.textView2);
-        if (readFile.exists() && readFile.canRead()) {
-        	FileInputStream fis = null;
-        	try {
-        		fis = new FileInputStream(readFile);
-        		byte[] reader = new byte[fis.available()];
-        		while (fis.read(reader) != -1) {
-				}
-				t.setText("Backup Path: /sdcard/" + new String(reader));
-        	} catch (IOException e) {
-        		Log.e("COT", e.getMessage(), e);
-        	} finally {
-        		if (fis != null) {
-					try {
-						fis.close();
-					} catch (IOException e) {
-					}
-				}
-        	}
-        } else {
-        	t.setText("Backup Path: /sdcard/cotrecovery");
-        }
-		
+        UpdateLabel();
     }
 
     @Override
@@ -132,8 +109,6 @@ public class MainActivity extends Activity {
 			alert.setView(input);
 			alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int whichButton) {
-					TextView t = new TextView(MainActivity.this);
-					t = (TextView)findViewById(R.id.textView2);
 					String fileName = ".userdefinedbackups";
 					File sdDir = new File(Environment.getExternalStorageDirectory().getPath());
 					if (sdDir.exists() && sdDir.canWrite()) {
@@ -150,7 +125,7 @@ public class MainActivity extends Activity {
 							try {
 								fos = new FileOutputStream(file);
 								fos.write(input.getText().toString().trim().getBytes());
-								t.setText("Backup Path: /sdcard/" + input.getText().toString().trim());
+								UpdateLabel();
 							} catch (FileNotFoundException e) {
 								Log.e("COT", "ERROR", e);
 							} catch (IOException e) {
@@ -276,6 +251,80 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    }
+    
+    public void UpdateLabel() {
+    	String fileName = ".userdefinedbackups";
+        File sdDir = new File(Environment.getExternalStorageDirectory().getPath());
+        File readFile = new File(sdDir.getPath() + "/cotrecovery/" + fileName);
+    	String orsreboot = null;
+    	String orswipeprompt = null;
+    	String backupprompt = null;
+    	String sigcheck = null;
+    	String iniFile = "settings.ini";
+        InputStream readIniFile;
+		try {
+			readIniFile = new FileInputStream(sdDir.getPath() + "/cotrecovery/" + iniFile);
+			IniStreamReader ini = new IniStreamReader(new InputStreamReader(readIniFile));
+			while(ini.readNext()) {
+				final String key = ini.getKey();
+				final String value = ini.getValue();
+				final String section = ini.getSection();
+				if (section.startsWith("Settings")) {
+					if (key.equalsIgnoreCase("ORSReboot")) {
+						orsreboot = value.toString().trim();
+						orsreboot = orsreboot.replace(";", "");
+						orsreboot = orsreboot.trim();
+					} else if (key.equalsIgnoreCase("ORSWipePrompt")) {
+						orswipeprompt = value.toString().trim();
+						orswipeprompt = orswipeprompt.replace(";", "");
+						orswipeprompt = orswipeprompt.trim();
+					} else if (key.equalsIgnoreCase("BackupPrompt")) {
+						backupprompt = value.toString().trim();
+						backupprompt = backupprompt.replace(";", "");
+						backupprompt = backupprompt.trim();
+					} else if (key.equalsIgnoreCase("SignatureCheckEnabled")) {
+						sigcheck = value.toString().trim();
+						sigcheck = sigcheck.replace(";", "");
+						sigcheck = sigcheck.trim();
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	TextView t = new TextView(this);
+		t = (TextView)findViewById(R.id.textView2);
+        if (readFile.exists() && readFile.canRead()) {
+        	FileInputStream fis = null;
+        	try {
+        		fis = new FileInputStream(readFile);
+        		byte[] reader = new byte[fis.available()];
+        		while (fis.read(reader) != -1) {
+				}
+				t.setText("Backup Path: /sdcard/" + new String(reader) + "\n");
+        	} catch (IOException e) {
+        		Log.e("COT", e.getMessage(), e);
+        	} finally {
+        		if (fis != null) {
+					try {
+						fis.close();
+					} catch (IOException e) {
+					}
+				}
+        	}
+        } else {
+    		t.setText("Backup Path: /sdcard/cotrecovery\n");
+        }
+        t.append("GooManager Forced Reboots: " + orsreboot + "\n");
+		t.append("GooManager Partition Wipe Prompt: " + orswipeprompt + "\n");
+		t.append("ZIP Flash Nandroid Prompt: " + backupprompt + "\n");
+		t.append("ZIP Integrity Checking: " + sigcheck + "\n");
+    	
     }
     
 }
